ea56598e219ae64b61000f1e3f32c065821ac8e2
dustin heisey <dustin@heisey.email>
2021-03-30 19:19:57 -0700

    progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,23 @@
-<form class="sidebar" min="1/" space="xl" onsubmit="return false;">
-  <input type="text" />
-  <button class="fixed">Search</button>
-</form>
+<section class="cover">
+  <nav>
+    <img src="" alt="" class="inline" />
+    <ul class="inline">
+      <li>list 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+      <li>item 4</li>
+      <li>item 5</li>
+    </ul>
+  </nav>
+  <div class="stack" centered>
+    <div class="center">
+      <h1>jlskjf sdkljf sdf sd f</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ducimus quod vero ipsam
+        natus quos mollitia est autem, ipsa laudantium temporibus exercitationem assumenda
+        praesentium amet, aspernatur magni aperiam eligendi sit!
+      </p>
+    </div>
+  </div>
+  <div class="center"><button>hello</button></div>
+</section>
