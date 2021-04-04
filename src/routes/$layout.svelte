3fd1fef3b9bf4bef2f8628191bf4f083ba7e5777
dustin heisey <dustin@heisey.email>
2021-04-03 19:37:29 -0700

    inset

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,14 +2,11 @@
   import '$styles/reset.css'
   import '$styles/tokens.css'
 
-  import '$layout/justify.css'
+  import '$layout/inset.css'
 
-  import '$layout/box.css'
   import '$layout/center.css'
   import '$layout/cover.css'
-  import '$layout/frame.css'
   import '$layout/grid.css'
-  import '$layout/icon.css'
   import '$layout/inline.css'
   import '$layout/masonry.css'
   import '$layout/reel.css'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,18 @@
-<div class="box" space="m">
-  <div class="center" style="--max: 60ch">
-    <div class="stack" space="l">
-      <div class="sidebar" gap="xs">
-        <div>
-          <input type="text" />
-          <button fixed>Copy</button>
-        </div>
+<div class="center" inset="m" style="--max: 60ch">
+  <div class="stack" space="l">
+    <div class="sidebar" gap="xs">
+      <div>
+        <input type="text" />
+        <button fixed>Copy</button>
       </div>
-      <div class="stack" space="s">
-        <h2>Distribute</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel ullam velit aliquam
-          expedita, totam dolorum sunt, qui delectus, molestias animi culpa earum in amet voluptatem
-          perferendis. Voluptatum ipsam illo quaerat.
-        </p>
-      </div>
+    </div>
+    <div class="stack" inset="xs" space="s">
+      <h2>Distribute</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel ullam velit aliquam expedita,
+        totam dolorum sunt, qui delectus, molestias animi culpa earum in amet voluptatem
+        perferendis. Voluptatum ipsam illo quaerat.
+      </p>
     </div>
   </div>
 </div>
