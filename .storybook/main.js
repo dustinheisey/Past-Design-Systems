f279d60c69d79053cb53948d1f00c11e63cd5719
Dustin Heisey <dustin@heisey.email>
2022-06-08 12:33:05 -0500

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+    stories: ['../src/components/**/*.story.mdx'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-postcss'
+    ],
+    framework: '@storybook/html',
+    core: {
+        disableTelemetry: true
+    }
+}
