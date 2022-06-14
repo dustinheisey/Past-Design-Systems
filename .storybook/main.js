4d63bfb06b442693c1868e8c08d2cdaf0bb15e39
Dustin Heisey <dustin@heisey.email>
2022-06-14 15:56:30 -0500

    setup design tokens

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,15 @@ module.exports = {
             directory: '../src/components/containers/stories',
             titlePrefix: 'Containers/',
             files: '*.story.*'
+        },
+        {
+            directory: '../src/global/styleguide',
+            titlePrefix: 'Style Guide/',
+            files: '*.story.*'
         }
     ],
     addons: [
+        'storybook-design-token',
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
