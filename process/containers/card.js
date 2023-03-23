21223becd1473077f771d45949d48947f4629de4
Dustin Heisey <dustin@heisey.email>
2023-03-23 08:53:14 -0700

    footer & index

--- process/containers/card.js
+++ process/containers/card.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const MyComponent = () => {
 	return (
+		
 		<div className="flex flex-col items-center justify-center h-screen bg-gray-200">
 			<div className="bg-white p-10 rounded-lg shadow-lg">
 				<h1 className="text-3xl font-bold mb-4">Hello World</h1>
