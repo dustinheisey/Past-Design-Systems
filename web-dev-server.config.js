8de18f360bcba17315c9af4b610905c3e6645e26
Dustin Heisey <dustin@heisey.email>
2023-04-22 11:41:41 -0700

    get storybook working

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,10 +1,4 @@
-/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-import {legacyPlugin} from '@web/dev-server-legacy';
+import { legacyPlugin } from '@web/dev-server-legacy';
 
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
