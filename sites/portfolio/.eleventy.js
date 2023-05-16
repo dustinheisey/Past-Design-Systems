975410109bd6c77882229521214071ed83d9be00
Dustin Heisey <dustin@heisey.email>
2023-05-16 15:05:43 -0700

    cleanup

--- sites/portfolio/.eleventy.js
+++ sites/portfolio/.eleventy.js
@@ -1,38 +1,11 @@
-const Image = require("@11ty/eleventy-img");
-
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [300, 600, 1200],
-    formats: ["avif", "webp", "jpeg"],
-    urlPath: "/assets/images",
-    outputDir: "./_site/assets",
-  });
-
-  let image = Image.generateHTML(metadata, {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  });
-
-  return image;
-}
-
-function svgShortcode(src, alt, className) {
-  return `<img src="${src}" alt="${alt}" class="${className || ""}">`;
-}
+// const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addGlobalData("navigation", "./_data/navigation.json");
+  // eleventyConfig.addPlugin(EleventyVitePlugin);
 
   eleventyConfig.addLayoutAlias("base", "base.njk");
 
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addShortcode("svg", svgShortcode);
-
   eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("site.webmanifest");
-  eleventyConfig.addPassthroughCopy("service-worker.js");
   eleventyConfig.addPassthroughCopy("dark-mode.js");
   eleventyConfig.addPassthroughCopy("range.js");
   eleventyConfig.addPassthroughCopy("navbar.js");
