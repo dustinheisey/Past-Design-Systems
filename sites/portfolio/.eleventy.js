ba737eeef37c0b8c77e60091d8cc0e2e6602900b
Dustin Heisey <dustin@heisey.email>
2023-05-16 18:53:42 -0700

    potentially fix white space issue

--- sites/portfolio/.eleventy.js
+++ sites/portfolio/.eleventy.js
@@ -1,7 +1,19 @@
 // const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const Nunjucks = require("nunjucks");
 
 module.exports = function (eleventyConfig) {
   // eleventyConfig.addPlugin(EleventyVitePlugin);
+
+  const nunjucksEnvironment = new Nunjucks.Environment(
+    // Specify the directories where your templates reside.
+    new Nunjucks.FileSystemLoader(["./", "_includes", "_layouts"]),
+
+    // Apply rendering options
+    {
+      lstripBlocks: true,
+      trimBlocks: true,
+    }
+  );
 
   eleventyConfig.addLayoutAlias("base", "base.njk");
 
@@ -10,4 +22,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("range.js");
   eleventyConfig.addPassthroughCopy("navbar.js");
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
+  return {
+    dir: {
+      // These values are both relative to your input directory.
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
 };
