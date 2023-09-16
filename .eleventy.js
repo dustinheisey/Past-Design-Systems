9af7b24f9b18d80105e5e80c8e7bb34458f5c3b8
Dustin Heisey <dustin@heisey.email>
2023-09-16 17:38:53 -0500

    add optional blog functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,41 @@
 const common = require("@dh253/nds");
 
+// OPTIONAL
+const fs = require("fs");
+const Image = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(common, {
     domain: "fixme",
+  });
+
+  // OPTIONAL
+  eleventyConfig.on("afterBuild", () => {
+    const socialPreviewImagesDir = "_site/img/og/";
+    fs.readdir(socialPreviewImagesDir, (err, files) => {
+      if (files.length > 0) {
+        files.forEach((filename) => {
+          if (filename.endsWith(".svg")) {
+            let imageUrl = socialPreviewImagesDir + filename;
+            Image(imageUrl, {
+              formats: ["jpeg"],
+              outputDir: "./" + socialPreviewImagesDir,
+              filenameFormat: (id, src, width, format, options) => {
+                let outputFilename = filename.substring(0, filename.length - 4);
+                return `${outputFilename}.${format}`;
+              },
+            })
+              .then(() => {
+                fs.unlink(imageUrl, (err) => {
+                  if (err) throw err;
+                });
+              })
+              .catch((error) => {
+                console.error(`Failed to process image: ${error}`);
+              });
+          }
+        });
+      }
+    });
   });
 };
---
+++ posts/OPTIONAL.md
@@ -0,0 +1,8 @@
+---
+title: "OPTIONAL"
+description: "OPTIONAL"
+url: OPTIONAL
+date: 2023-06-07T19:43:48.508Z
+---
+
+# OPTIONAL
